8066e077850a68b078281a2b0eb61bfda377125c
Serkuu <kutnysergiusz@gmail.com>
2026-05-26 22:12:53 +0200

    OmitType do macAddress

--- backend/src/head-unit/dto/update-head-unit.dto.ts
+++ backend/src/head-unit/dto/update-head-unit.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateHeadUnitDto } from './create-head-unit.dto';
 
-export class UpdateHeadUnitDto extends PartialType(CreateHeadUnitDto) {}
+export class UpdateHeadUnitDto extends PartialType(OmitType(CreateHeadUnitDto, ['macAddress'] as const)) { }
--- backend/src/sensor/dto/update-sensor.dto.ts
+++ backend/src/sensor/dto/update-sensor.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateSensorDto } from './create-sensor.dto';
 
-export class UpdateSensorDto extends PartialType(CreateSensorDto) {}
+export class UpdateSensorDto extends PartialType(OmitType(CreateSensorDto, ['macAddress'] as const)) { }
